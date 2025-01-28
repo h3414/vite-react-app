fdbe2f17b868e1573feca43d03681916fe412d2a
h3414 <harishreddy4456@gmail.com>
2025-01-28 22:16:05 +0530

    thsisiDSS

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1> thsi is the first change tusus final update therh
+      <h1>Vite + React</h1> t
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           {" "}
