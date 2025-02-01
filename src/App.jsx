4c2b9734e2d36c863b735869b912b96be8a46c12
h3414 <harishreddy4456@gmail.com>
2025-02-01 23:14:21 +0530

    this is the updated  code

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           thuisis count count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR updated
         </p>
       </div>
       <p className="read-the-docs">
